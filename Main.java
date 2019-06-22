92e58b83345df4104f1abc4bed20f6925463196a
Mafu <36803542+darealmafu@users.noreply.github.com>
2019-06-22 18:44:47 -0400

    epic

---
+++ Main.java
@@ -0,0 +1,26 @@
+package zone.mafu;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Starting number?");
+        int starting = sc.nextInt();
+        System.out.println("Ending number?");
+        int ending = sc.nextInt();
+
+        for(int i = starting; i <= ending; i ++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            } else if(i % 3 == 0){
+                System.out.println("Fizz");
+            } else if(i % 5 == 0){
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
